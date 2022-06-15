7cbc0645ebb58b6135423754f598a6bd2863a281
Juanzitozito <juan.cordova@hotmail.com>
2022-06-15 14:43:47 -0300

    mudança estrutura

--- script/scriptnews.js
+++ script/scriptnews.js
@@ -2,9 +2,13 @@
 
 onload = () => {
 const btnPerfil = document.getElementById('perfil')
+const noticia = document.getElementsByClassName('noticia')
 
 btnPerfil.addEventListener('click', () => {
       window.location.href = 'profile.html'
 })
 
+noticia.addEventListener('click', () => {
+      window.location.href = 'noticia.html'
+})
 }
