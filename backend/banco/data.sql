470eb96daf3a2e077e14ad39631fe9233809b4fc
Juan Cordova <juan.170105@bento.local>
2022-06-28 14:08:33 -0300

    organização

---
+++ backend/banco/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO usuario(email, nome, nick, dtnasc, cargo) VALUES('user@user.com', 'user', 'manito', '2022-01-01', 'usuário');
+INSERT into noticia(imagem, titulo, descricao, conteudo, dataPublicacao) VALUES ('asdsad', 'asdsadsd', 'asdsadsad', 'asdsadsad', '2022-01-01');
--- banco/data.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO usuario(email, nome, nick, dtnasc, cargo) VALUES('user@user.com', 'user', 'manito', '2022-01-01', 'usuário');
